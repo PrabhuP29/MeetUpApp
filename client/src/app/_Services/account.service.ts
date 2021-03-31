1a39d9227a1420d849c9cb61f3965e6359efc82e
Prabhu Pattnaik <rudolph2006@gmail.com>
2021-03-30 21:43:42 -0400

    Photo-Editor & Cloudinary

--- client/src/app/_Services/account.service.ts
+++ client/src/app/_Services/account.service.ts
@@ -21,7 +21,8 @@ export class AccountService {
         const user = response;
         if(user){
           localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          //this.currentUserSource.next(user);
+          this.setCurrentUser(user);
           return response;
         }
       })
@@ -42,7 +43,8 @@ export class AccountService {
       map((user: User) => {
         if(user){
           localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          //this.currentUserSource.next(user);
+          this.setCurrentUser(user);
           return user;
         }
       })
